b2ceb2e6d58e2e9e329fcee5f99cbe852d1b991d
loumoore <lourmoore@gmail.com>
2020-09-25 10:34:41 -1000

    add safety net category

---
+++ _data/sites/safetyNet.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "Safety Net", // optional, falls back to object key
+	description: "Safety Net",
+	options: {
+		frequency: 60 * 4, // (in minutes), 4 hours
+	},
+	urls: [
+		"https://nheasy.nh.gov/",
+    "https://www.mybenefitscalwin.org/",
+    "https://www.c4yourself.com/",
+    "https://www.yourbenefits.laclrs.org/"
+	]
+};
