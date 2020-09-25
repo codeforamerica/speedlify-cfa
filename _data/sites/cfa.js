7489c700ed99f44794b0a2a6e56f8a8ac19fb0c0
loumoore <lourmoore@gmail.com>
2020-09-25 00:37:12 -1000

    consolidate into one category

---
+++ _data/sites/cfa.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Code for America", // optional, falls back to object key
+	description: "Code for America",
+	options: {
+		frequency: 60 * 4, // (in minutes), 4 hours
+	},
+	urls: [
+		"https://www.getcalfresh.org/",
+		"https://www.getyourrefund.org/",
+		"https://www.mnbenefits.org/"		
+	]
+};
